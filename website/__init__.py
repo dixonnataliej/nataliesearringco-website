871025697f7f35b133e39050effcd34f50f6cb91
dixonnataliej <dixonnataliej@gmail.com>
2024-12-21 15:39:10 -0700

    made index html template, nothing on it yet

--- website/__init__.py
+++ website/__init__.py
@@ -4,16 +4,8 @@ import flask
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 
-# Read settings from config module (insta485/config.py)
+# Read settings from config module (website/config.py)
 app.config.from_object('website.config')
-
-# Overlay settings read from a Python file whose path is set in the environment
-# variable INSTA485_SETTINGS. Setting this environment variable is optional.
-# Docs: http://flask.pocoo.org/docs/latest/config/
-#
-# EXAMPLE:
-# $ export INSTA485_SETTINGS=secret_key_config.py
-app.config.from_envvar('INSTA485_SETTINGS', silent=True)
 
 # Tell our app about views and model.  This is dangerously close to a
 # circular import, which is naughty, but Flask was designed that way.
