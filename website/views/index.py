29fb9e804a46c9e8c16cea0815e9ed1a49326fcd
dixonnataliej <dixonnataliej@gmail.com>
2024-12-22 11:38:21 -0700

    added a dynamic post name list to website to check database functionality. will switch to client side later

--- website/views/index.py
+++ website/views/index.py
@@ -11,5 +11,13 @@ import website
 @website.app.route('/')
 def show_index():
     """Display / route."""
-    context = {}
+    connection = website.model.get_db()
+
+    cur = connection.execute(
+        "SELECT name "
+        "FROM posts"
+    )
+    post_names = cur.fetchall()
+
+    context = {"post_names": post_names}
     return flask.render_template("index.html", **context)
