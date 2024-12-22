5d67a6607d4cd720d931c913dd18397129af8567
dixonnataliej <dixonnataliej@gmail.com>
2024-12-22 11:20:36 -0700

    working on database, uploads, posts, tags, creating and inserting base data into tables

---
+++ sql/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO posts(postid, filename, name, price, description, status)
+VALUES (1, 'rainbow_earrings_2948.jpg', 'Rainbow Star Earrings', '$10',
+        'These sure' || ' are rainbow star earrings', 'Made to Order'),
+       (2, 'blue_earrings_purple_12y87.jpg', 'Blue Purple Circles', '$10',
+       'description pending', 'Available'),
+       (3, 'pastel_butterfly_esque_earrings_2389', 'Pastel Butterfly Esque' ||
+       ' Earrings', '$15', 'description tbd', 'Sold Out');
+
+INSERT INTO tags(tagid, name)
+VALUES (1, '$10 or less'),
+       (2, 'pride');
+
+INSERT INTO post_tags(postid, tagid)
+VALUES (1, 1),
+       (1, 2),
+       (2, 1);
