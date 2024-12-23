dbc9d5159cde4dce9c7df4c3ffb1d6ed9556f895
dixonnataliej <dixonnataliej@gmail.com>
2024-12-23 13:55:13 -0700

    started adding javascript files

---
+++ website/js/main.jsx
@@ -0,0 +1,14 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Gallery from "./gallery";
+
+// Create a root
+const root = createRoot(document.getElementById("reactEntry"));
+
+// This method is only called once
+// Insert the post component into the DOM
+root.render(
+  <StrictMode>
+    <Gallery />
+  </StrictMode>,
+);
